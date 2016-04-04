d0ece5ed67bf26b4389d3395a5241a0e6d227c8b
CarlosTrancon <carlostala8@gmail.com>
2016-04-04 13:31:13 +0200

    inplementacion calculadora

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,5 +1,17 @@
 package calculadora;
 
 public class Calculadora {
+	private int Numero1;
+	private int Numero2;
 	
+	public Calculadora(int n1, int n2){
+		this.Numero1 = n1;
+		this.Numero2 = n2;
+	}
+	public int suma(){
+		return this.Numero1 + this.Numero2;
+	}
+	public double division(){
+		return (double)this.Numero1/this.Numero2;
+	}
 }
